bb45356708dbf2b1145e55dd8396e028c84fa4a2
Kenechukwu <kiloeje@byteworks.com.ng>
2022-10-12 20:28:40 -0400

    setup updates

---
+++ src/main/java/com/kene/esdiactest/EsdiacTestApplication.java
@@ -0,0 +1,13 @@
+package com.kene.esdiactest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdiacTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsdiacTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kene/esdiactest/EsdiacTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kene.esdiactest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdiacTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
